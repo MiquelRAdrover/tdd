93d969abb57876cf3fcb491c487d190e5d531ec1
MiquelRAdover <miquelrossello847@gmail.com>
2026-04-13 12:35:57 +0200

    Practica 6 Paso 3 refactorizacion

--- src/main/java/mrossello/tdd/Cuenta.java
+++ src/main/java/mrossello/tdd/Cuenta.java
@@ -9,20 +9,22 @@ public class Cuenta {
 
 
 	public void ingresar(double ingreso) {
-		if (ingreso > 0 || ingreso > 3000) {
+		if (ingreso > 0 && ingreso < 3001) {
 			this.saldo += ingreso;
 		}
 	}
 
 	public void retirar(double retirada) {
-		if (retirada > 0 || retirada < 3000 && retirada <= saldo) {
+		if (retirada > 0 && retirada < 3001 && retirada <= saldo) {
 			this.saldo -= retirada;
 		}
 	}
 
 	public void transferencia(double dineroTransferencia, Cuenta cuentaReceptora) {
-		retirar(dineroTransferencia);
-		cuentaReceptora.ingresar(dineroTransferencia);
+		if (dineroTransferencia < 1501) {
+			retirar(dineroTransferencia);
+			cuentaReceptora.ingresar(dineroTransferencia);
+		}
 	}
 
 	public double getSaldo() {
--- src/test/java/mrossello/tdd/TddApplicationTests.java
+++ src/test/java/mrossello/tdd/TddApplicationTests.java
@@ -46,6 +46,23 @@ class TddApplicationTests {
 	}
 
 	@Test
+	void ingresar3000EnCuentaVacia() {
+		Cuenta cuenta = new Cuenta();
+		double ingreso = 3000;
+		cuenta.ingresar(ingreso);
+		Assertions.assertEquals(ingreso, cuenta.saldo);
+	}
+
+	@Test
+	void ingresar3001EnCuentaVaciaNoPasaNada() {
+		Cuenta cuenta = new Cuenta();
+		double ingreso = 3001;
+		cuenta.ingresar(ingreso);
+		Assertions.assertEquals(0, cuenta.saldo);
+	}
+
+
+	@Test
 	void retiradaDinero() {
 		Cuenta cuenta = new Cuenta();
 		double ingreso = 1000;
@@ -53,6 +70,58 @@ class TddApplicationTests {
 		cuenta.ingresar(ingreso);
 		cuenta.retirar(retirada);
 		Assertions.assertEquals(0, cuenta.saldo);
+	}
+
+	@Test
+	void retirar10EnUnaCuentaDe500Esperamos490() {
+		Cuenta cuenta = new Cuenta();
+		double ingreso = 500;
+		double retirada = 10;
+		cuenta.ingresar(ingreso);
+		cuenta.retirar(retirada);
+		Assertions.assertEquals(ingreso - retirada, cuenta.saldo);
+	}
+
+	@Test
+	void retirar300EnUnaCuentaDe100INoPasaNada() {
+		Cuenta cuenta = new Cuenta();
+		double ingreso = 100;
+		double retirada = 300;
+		cuenta.ingresar(ingreso);
+		cuenta.retirar(retirada);
+		Assertions.assertEquals(ingreso, cuenta.saldo);
+	}
+
+	@Test
+	void retirar10EnNegativoEnUnaCuentaDe300ResultadoEsperado300() {
+		Cuenta cuenta = new Cuenta();
+		double ingreso = 300;
+		double retirada = -10;
+		cuenta.ingresar(ingreso);
+		cuenta.retirar(retirada);
+		Assertions.assertEquals(ingreso, cuenta.saldo);
+	}
+
+	@Test
+	void retirar3000EnUnaCuentaDe4000() {
+		Cuenta cuenta = new Cuenta();
+		double ingreso = 2000;
+		double retirada = 3000;
+		cuenta.ingresar(ingreso);
+		cuenta.ingresar(ingreso);
+		cuenta.retirar(retirada);
+		Assertions.assertEquals((ingreso + ingreso) - retirada, cuenta.saldo);
+	}
+
+	@Test
+	void retirar3001EnUnaCuentaDe4000ResultadoEsperado4000() {
+		Cuenta cuenta = new Cuenta();
+		double ingreso = 2000;
+		double retirada = 3001;
+		cuenta.ingresar(ingreso);
+		cuenta.ingresar(ingreso);
+		cuenta.retirar(retirada);
+		Assertions.assertEquals((ingreso + ingreso), cuenta.saldo);
 	}
 
 	@Test
@@ -65,4 +134,61 @@ class TddApplicationTests {
 		Assertions.assertEquals(290, cuenta.saldo);
 		Assertions.assertEquals(410, cuenta2.saldo);
 	}
+
+	@Test
+	void tranferenciaDe100DesdeUnaCuentaCon300AUnaCon400() {
+		Cuenta cuenta = new Cuenta();
+		Cuenta cuenta2 = new Cuenta();
+		cuenta.ingresar(300);
+		cuenta2.ingresar(400);
+		cuenta.transferencia(100, cuenta2);
+		Assertions.assertEquals(200, cuenta.saldo);
+		Assertions.assertEquals(500, cuenta2.saldo);
+	}
+
+	@Test
+	void tranferenciaDe10EnNegativoDesdeUnaCuentaCon300AUnaCon40() {
+		Cuenta cuenta = new Cuenta();
+		Cuenta cuenta2 = new Cuenta();
+		cuenta.ingresar(300);
+		cuenta2.ingresar(40);
+		cuenta.transferencia(-10, cuenta2);
+		Assertions.assertEquals(300, cuenta.saldo);
+		Assertions.assertEquals(40, cuenta2.saldo);
+	}
+
+	@Test
+	void tranferenciaDe1500DesdeUnaCuentaCon2500AUnaCon500() {
+		Cuenta cuenta = new Cuenta();
+		Cuenta cuenta2 = new Cuenta();
+		cuenta.ingresar(2500);
+		cuenta2.ingresar(500);
+		cuenta.transferencia(1500, cuenta2);
+		Assertions.assertEquals(1000, cuenta.saldo);
+		Assertions.assertEquals(2000, cuenta2.saldo);
+	}
+
+	@Test
+	void tranferenciaDe1501DesdeUnaCuentaCon2500AUnaCon500() {
+		Cuenta cuenta = new Cuenta();
+		Cuenta cuenta2 = new Cuenta();
+		cuenta.ingresar(2500);
+		cuenta2.ingresar(500);
+		cuenta.transferencia(1501, cuenta2);
+		Assertions.assertEquals(2500, cuenta.saldo);
+		Assertions.assertEquals(500, cuenta2.saldo);
+	}
+
+	@Test
+	void tranferenciaDe1000DesdeUnaCuentaCon2500AUnaCon500YUnaSegundaTransferenciaDe1200() {
+		Cuenta cuenta = new Cuenta();
+		Cuenta cuenta2 = new Cuenta();
+		cuenta.ingresar(2500);
+		cuenta2.ingresar(500);
+		cuenta.transferencia(1000, cuenta2);
+		cuenta.transferencia(1200, cuenta2);
+		Assertions.assertEquals(300, cuenta.saldo);
+		Assertions.assertEquals(2700, cuenta2.saldo);
+	}
+
 }
